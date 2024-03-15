e22f2f0ba4715fae88368f955c9d3db26115620d
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-15 22:08:35 +0900

    [Bronze V] Title: 고양이, Time: 4 ms, Memory: 808 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.go
+++ 백준/Bronze/10171. 고양이/고양이.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// 여러 문자열보다 이렇게 하는게 더 빠르구먼, 그리고 여러 문자열 쓸때는 \\ 두개 안해줬어도 되었는데, 이렇게 출력하면 \\해줘야하는구먼
 func main() {
     fmt.Println("\\    /\\")
     fmt.Println(" )  ( ')")
