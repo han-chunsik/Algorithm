ba20406d3eb6a6bbf4387c5cb91ea62e358ab1d2
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-17 12:18:55 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,7 +1,9 @@
 a = int(input())
 b = int(input())
 
-if(0 < a and 0 < b):print(1)
-elif(a < 0 and 0 < b):print(2)
-elif(a < 0 and b < 0):print(3)
-else:print(4)
+if 0 < a:
+    if 0 < b:print(1)
+    else:print(4)
+else:
+    if 0 < b:print(2)
+    else:print(3)
