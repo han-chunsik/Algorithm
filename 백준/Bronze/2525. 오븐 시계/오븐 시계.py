5daa53fe9d9554a7a62a4fc93d68bafd217f1ada
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-17 13:43:09 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,20 +1,3 @@
 h, m = map(int, input().split())
 t = int(input())
-
-if 60 <= t:
-    h+=(t // 60)
-    m+=(t % 60)
-    if 60 <= m:
-        h+=1
-        m-=60
-    if 24 <= h:
-        h-=24
-else:
-    m+=t
-    if 60 <= m:
-        h +=1
-        m-=60
-    if 24 <= h:
-        h-=24
-
-print(h, m)   
+print((h+(m+t)//60)%24, (m+t)%60)
