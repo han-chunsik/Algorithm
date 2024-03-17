25778713132bdd2c2af38742b94dcde5fca3e6a0
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-17 12:55:21 +0900

    [Bronze III] Title: 알람 시계, Time: 4 ms, Memory: 864 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var h, m int
+
+	fmt.Scan(&h, &m)
+
+	if m-45 < 0 {
+		m = 60 + (m - 45)
+		if h == 0 {
+			h = 23
+		} else {
+			h -= 1
+		}
+	} else {
+		m -= 45
+	}
+
+	fmt.Print(h, m)
+}
