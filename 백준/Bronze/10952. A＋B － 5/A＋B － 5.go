1bfd7ced322515f2b0ea7907e5cd20ed8665131d
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-22 21:49:04 +0900

    [Bronze V] Title: A+B - 5, Time: 24 ms, Memory: 872 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.go
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	var a, b int
 
-	for i := 0; i < 1; {
+	for {
 		fmt.Scanln(&a, &b)
 		if a == 0 && b == 0 {
-			i += 1
+			break
 		} else {
 			fmt.Println(a + b)
 		}
