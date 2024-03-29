aac0cbffa22b293a974c610db9fac1afb49c6a25
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-29 22:25:05 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,14 +1,10 @@
 n, m = map(int,input().split())
-b = []
 
-for x in range(1, n+1):
-    b.append(x)
+buckets = list(range(1,n+1))
 
 for z in range(m):
     i,j = map(int, input().split())
-    num = b[i-1]
-    b[i-1] = b[j-1]
-    b[j-1] = num
+    buckets[i-1], buckets[j-1] = buckets[j-1], buckets[i-1] 
 
 for k in range(n):
-    print(b[k], end=" ")
+    print(buckets[k], end=" ")
