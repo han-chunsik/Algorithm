8b193f719ab8246898ac74c6f7b5520ddc1129f0
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-04-06 22:06:14 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,17 +1,3 @@
-import string
 s = input()
-a_list = list(string.ascii_lowercase)
-
-result_list = []
-
-for a in a_list:
-    if a in s:
-        for i, c in enumerate(s):
-            if a == c:
-                result_list.append(i)
-                break
-    else:
-        result_list.append(-1)
-        
-for k in range(len(result_list)):
-    print(result_list[k], end=" ")
+for i in range(97,123):
+    print(s.find(chr(i)), end=" ")
