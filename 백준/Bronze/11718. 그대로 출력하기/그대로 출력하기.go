1f8732dad762616c6efd70031168630222cd3946
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-04-07 17:39:07 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 828 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.go
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	s := bufio.NewScanner(os.Stdin)
-
-	for s.Scan() {
-		text := s.Text()
-		fmt.Println(text)
-
-		if s.Err() == io.EOF {
-			break
-		}
-	}
-
+	io.Copy(os.Stdout, os.Stdin)
 }
