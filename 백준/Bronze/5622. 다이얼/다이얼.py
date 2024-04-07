5755d3384988d4ced0e7b9600531de0709a51f82
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-04-07 16:49:13 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -2,23 +2,21 @@ s = input()
 t = 0
 
 for j in s:
-    chr = ord(j)
-    n = 0
-    if 65 <= chr <= 67:
+    if 'A' <= j <= 'C':
         n = 2
-    elif 68 <= chr <= 70:
+    elif 'D' <= j <= 'F':
         n = 3
-    elif 71 <= chr <= 73:
+    elif 'G' <= j <= 'I':
         n = 4
-    elif 74 <= chr <= 76:
+    elif 'J' <= j <= 'L':
         n = 5
-    elif 77 <= chr <= 79:
+    elif 'M' <= j <= 'O':
         n = 6
-    elif 80 <= chr <= 83:
+    elif 'P' <= j <= 'S':
         n = 7
-    elif 84 <= chr <= 86:
+    elif 'T' <= j <= 'V':
         n = 8
-    elif 87 <= chr <= 90:
+    elif 'W' <= j <= 'Z':
         n = 9
     t += n+1
 print(t)
