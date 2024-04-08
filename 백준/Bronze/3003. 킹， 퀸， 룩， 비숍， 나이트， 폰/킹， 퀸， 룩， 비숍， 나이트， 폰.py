880c06c56861177af8c6774450b72e6232a8aeeb
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-04-08 22:26:42 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,15 +1,2 @@
-chess = [1,1,2,2,2,8]
-
-nums = input().split()
-
-result = []
-
-for i in range(6):
-    if chess[i] < int(nums[i]):
-        a = abs(chess[i] - int(nums[i])) * -1
-    else:
-        a = abs(chess[i] - int(nums[i]))
-    result.append(a)
-
-for j in result:
-    print(j,end=" ")
+a,b,c,d,e,f=map(int,input().split())
+print(1-a,1-b,2-c,2-d,2-e,8-f)
