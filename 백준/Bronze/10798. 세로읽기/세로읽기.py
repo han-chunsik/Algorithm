44cdf30b89489f5bb0151894455498b63629ee94
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-04-14 00:04:39 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,18 +1,11 @@
-mtx = []
-max_len = 0
+ws = []
 for i in range(5):
-    line = list(input().replace(""," ").split())
-    if max_len < len(line):
-        max_len = len(line)
-    mtx.append(line)
-
-for i in mtx:
-    while len(i) < max_len:
-        i.extend(["@"])
+    ws.append(input())
 
 result = ""
-for i in range(max_len):
+for i in range(15):
     for j in range(5):
-        result += mtx[j][i]
+        if len(ws[j]) > i:
+            result += ws[j][i]
 
-print(result.replace("@",""))
+print(result)
