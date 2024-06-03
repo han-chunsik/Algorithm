e9435ce90ca08b251b5a52d07c004b423207b207
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-06-03 22:55:45 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,12 +1,2 @@
-nums="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 n, b = input().split()
-
-n = n[::-1]
-b = int(b)
-
-sum = 0
-
-for i, n_str in enumerate(n):
-    nf = nums.find(n_str)
-    sum += nf*b**i
-print(sum)
+print(int(n,int(b)))
